e823707543c6b2716a02ab453571039a79148cdc
Matthijs <matthijs@plamola.com>
2014-12-17 08:00:48 +0100

    Duh, first sorting, then limiting the number of results

--- src/main/scala/nl/dekkr/hoppr/model/FetchLogger.scala
+++ src/main/scala/nl/dekkr/hoppr/model/FetchLogger.scala
@@ -16,26 +16,28 @@ case class FetchLog(id: Option[Int] = None, uri: String, result: Option[String] 
 object FetchLogger {
 
 
+  implicit val session = Schema.getSession
+
   implicit def toFetchRowToFetchRowLog(rows : List[FetchLogTable#TableElementType]) : List[FetchLog] = {
     for (row <- rows) yield
     new FetchLog(uri = row.uri, result = row.result,logdate = row.logdate,level = row.level)
 
   }
 
-  implicit val session = Schema.getSession
-
   def LogDebug(feedUri: String, fetchResult: String): Unit = writeToFetchLog(feedUri, fetchResult, Debug)
-  def LogInfo(feedUri: String, fetchResult: String): Int = writeToFetchLog(feedUri, fetchResult, Info)
-  def LogError(feedUri: String, fetchResult: String): Int = writeToFetchLog(feedUri, fetchResult, Error)
 
   private def writeToFetchLog(feedUri: String, fetchResult: String, level: LogLevel): Int = {
     TableQuery[Tables.FetchLogTable] += FetchLog(uri = feedUri, result = Option(fetchResult), level = level)
   }
 
-  def getLast100 : List[FetchLog] = TableQuery[Tables.FetchLogTable].take(100).sortBy(_.logdate desc).list
+  def LogInfo(feedUri: String, fetchResult: String): Int = writeToFetchLog(feedUri, fetchResult, Info)
+
+  def LogError(feedUri: String, fetchResult: String): Int = writeToFetchLog(feedUri, fetchResult, Error)
+
+  def getLast100: List[FetchLog] = TableQuery[Tables.FetchLogTable].sortBy(_.logdate desc).take(100).list
 
   // TODO filter on level (Critical, Error)
-  def getLast100Errors : List[FetchLog] = TableQuery[Tables.FetchLogTable].take(100).sortBy(_.logdate desc).list
+  def getLast100Errors: List[FetchLog] = TableQuery[Tables.FetchLogTable].sortBy(_.logdate desc).take(100).list
 
 
 
